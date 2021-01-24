4c70e37f75c0450ffc6b92ba05e2f226dbe093e2
daejin-lee <daejin.lee@nhn.com>
2021-01-24 16:52:06 +0900

    Create example08.js

---
+++ Chap07/example08.js
@@ -0,0 +1,13 @@
+console.log('1' + 2);
+console.log(1 + '2');
+
+console.log(1 + 2);
+
+console.log(1 + true);
+
+console.log(1 + false);
+
+console.log(1 + null);
+
+console.log(+undefined);
+console.log(1 + undefined);
