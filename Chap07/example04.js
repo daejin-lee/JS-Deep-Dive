956d876c6a21e69f8ccdfd0361371353e2e94c52
daejin-lee <daejin.lee@nhn.com>
2021-01-24 16:44:52 +0900

    Update example04.js

---
+++ Chap07/example04.js
@@ -0,0 +1,14 @@
+var x = 5;
+var result;
+
+result = x++;
+console.log(result, x);
+
+result = ++x;
+console.log(result, x);
+
+result = x--;
+console.log(result, x);
+
+result = --x;
+console.log(result, x);
