4b1ecd8c3e5c16eff35b4cf6e28bd8d66628d94b
daejin-lee <daejin.lee@nhn.com>
2021-01-24 16:48:12 +0900

    Create example06.js

---
+++ Chap07/example06.js
@@ -0,0 +1,15 @@
+var x = '1';
+console.log(typeof +x, +x);
+console.log(typeof x, x);
+
+x = true;
+console.log(typeof +x, +x);
+console.log(typeof x, x);
+
+x = false;
+console.log(typeof +x, +x);
+console.log(typeof x, x);
+
+x = 'Hello';
+console.log(typeof +x, +x);
+console.log(typeof x, x);
