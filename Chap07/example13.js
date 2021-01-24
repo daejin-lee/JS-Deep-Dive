54809a0d5cd6c0827e51f02387554d22c9165ee6
daejin-lee <daejin.lee@nhn.com>
2021-01-24 17:07:45 +0900

    Create example13.js

---
+++ Chap07/example13.js
@@ -0,0 +1,7 @@
+console.log('0' == ''); // false?
+console.log(0 == ''); // false?
+console.log(0 == '0'); // true?
+console.log(false == 'false'); // false?
+console.log(false == '0'); // true?
+console.log(false == null); // true?
+console.log(false == undefined); // false?
