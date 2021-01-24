e105263761c444b1e65c057c1ed10aae0d14f314
daejin-lee <daejin.lee@nhn.com>
2021-01-24 16:10:39 +0900

    Create example02.js

---
+++ Chap06/example02.js
@@ -0,0 +1,10 @@
+var binary = 0b01000001;
+var octal = 0o101;
+var hex = 0x41;
+
+console.log(typeof(binary));
+console.log(binary);
+console.log(typeof(octal));
+console.log(octal);
+console.log(typeof(hex));
+console.log(hex);
