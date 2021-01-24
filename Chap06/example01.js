56c9bbe5b1143cbd178ee36ee1b6778f60756c80
daejin-lee <daejin.lee@nhn.com>
2021-01-24 16:10:38 +0900

    Create example01.js

---
+++ Chap06/example01.js
@@ -0,0 +1,10 @@
+var integer = 10;
+var double = 10.12;
+var negative = -20;
+
+console.log(typeof(integer));
+console.log(integer);
+console.log(typeof(double));
+console.log(double);
+console.log(typeof(negative));
+console.log(negative);
