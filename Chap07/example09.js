0845878a6fe3275787098dd0cf99ef350fa6fd93
daejin-lee <daejin.lee@nhn.com>
2021-01-24 16:56:14 +0900

    Create example09.js

---
+++ Chap07/example09.js
@@ -0,0 +1,24 @@
+var x;
+
+x = 10;
+console.log('x = 10: ', x);
+
+x += 5;
+console.log('x += 5: ', x);
+
+x -= 5;
+console.log('x -= 5: ', x);
+
+x *= 5;
+console.log('x *= 5: ', x);
+
+x /= 5;
+console.log('x /= 5: ', x);
+
+x %= 5;
+console.log('x %= 5: ', x);
+
+var str = 'My name is ';
+
+str += 'Lee';
+console.log(str);
