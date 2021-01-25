76c8c9856ff987c2a2ea5c074cd20454a2573a17
daejin-lee <daejin.lee@nhn.com>
2021-01-25 09:43:22 +0900

    Create example36.js

---
+++ Chap07/example36.js
@@ -0,0 +1,6 @@
+console.log("2 ** 2 ** 2 = ", 2 ** 2 ** 2);
+console.log("Math.pow(Math.pow(2, 2), 2) = ", Math.pow(Math.pow(2, 2), 2));
+
+console.log("5 ** (3 ** 2) = ", 5 ** (3 ** 2));
+console.log("(5 ** 3) ** 2 = ", (5 ** 3) ** 2);
+console.log("5 ** 3 ** 2 = ", 5 ** 3 ** 2);
