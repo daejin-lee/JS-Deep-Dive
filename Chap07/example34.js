2e05c7ad73388d33babf9cf0687d676d483d2721
daejin-lee <daejin.lee@nhn.com>
2021-01-25 09:38:29 +0900

    Create example34.js

---
+++ Chap07/example34.js
@@ -0,0 +1,5 @@
+console.log("2 ** 2 = ", 2 ** 2);
+console.log("2 ** 2.5 = ", 2 ** 2.5);
+console.log("2 ** 0 = ", 2 ** 0);
+console.log("2 ** -2 = ", 2 ** -2);
+console.log("4 ** 0.5 = ", 4 ** 0.5);
