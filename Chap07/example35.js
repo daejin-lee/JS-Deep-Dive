70645a6e1b3802a16ac0e75525b355c7b91f266a
daejin-lee <daejin.lee@nhn.com>
2021-01-25 09:40:04 +0900

    Create example35.js

---
+++ Chap07/example35.js
@@ -0,0 +1,5 @@
+console.log("Math.pow(2, 2): ", Math.pow(2, 2));
+console.log("Math.pow(2, 2.5): ", Math.pow(2, 2.5));
+console.log("Math.pow(2, 0): ", Math.pow(2, 0));
+console.log("Math.pow(2, -2): ", Math.pow(2, -2));
+console.log("Math.pow(4, 0.5): ", Math.pow(4, 0.5));
