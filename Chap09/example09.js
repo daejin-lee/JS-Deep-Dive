e9ecfa76140287ae329f6fc0b25d3dd52c607a09
daejin-lee <daejin.lee@nhn.com>
2021-01-27 00:39:39 +0900

    Create example09.js

---
+++ Chap09/example09.js
@@ -0,0 +1,17 @@
+console.log(+'');
+console.log(+'0');
+console.log(+'1');
+console.log(+'string');
+
+console.log(+true);
+console.log(+false);
+
+console.log(+null);
+console.log(+undefined);
+
+// console.log(+Symbol());
+
+console.log(+{});
+console.log(+[]);
+console.log(+[10, 20]);
+console.log(+function(){});
