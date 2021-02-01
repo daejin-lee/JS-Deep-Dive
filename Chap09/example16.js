01b112d1b511d04e571d17c3b4d8a29388dc78f6
daejin-lee <daejin.lee@nhn.com>
2021-02-01 18:47:38 +0900

    Create example16.js

---
+++ Chap09/example16.js
@@ -0,0 +1,25 @@
+console.log("Boolean('x'): ", Boolean('x'));
+console.log("Boolean(''): ", Boolean(''));
+console.log("Boolean('false'): ", Boolean('false'));
+
+console.log("Boolean(0): ", Boolean(0));
+console.log("Boolean(1): ", Boolean(1));
+console.log("Boolean(NaN): ", Boolean(NaN));
+console.log("Boolean(Infinity): ", Boolean(Infinity));
+console.log("Boolean(null): ", Boolean(null));
+console.log("Boolean(undefined): ", Boolean(undefined));
+console.log("Boolean({}): ", Boolean({}));
+console.log("Boolean([]): ", Boolean([]));
+
+console.log("!!'x': ", !!'x');
+console.log("!!'': ", !!'');
+console.log("!!'false': ", !!'false');
+console.log("!!0: ", !!0);
+console.log("!!1: ", !!1);
+console.log("!!NaN: ", !!NaN);
+console.log("!!Infinity: ", !!Infinity);
+console.log("!!null: ", !!null);
+console.log("!!undefined: ", !!undefined);
+console.log("!!{}: ", !!{});
+console.log("!![]: ", !![]);
+
