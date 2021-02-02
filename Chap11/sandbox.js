2c421eeb4281a12ece7bc888d633980e00748657
daejin-lee <daejin.lee@nhn.com>
2021-02-02 13:29:09 +0900

    Create sandbox.js

---
+++ Chap11/sandbox.js
@@ -0,0 +1,20 @@
+var a = 11;
+var b = {
+    name: 'Lee',
+    age: 28
+};
+aa = a;
+bb = b;
+
+console.log(a);
+console.log(aa);
+console.log(b);
+console.log(bb);
+
+a = 22;
+b.age = 29;
+
+console.log(a);
+console.log(aa);
+console.log(b);
+console.log(bb);
