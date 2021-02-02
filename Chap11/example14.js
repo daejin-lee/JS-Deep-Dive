794c30c11af2ce9b5662dc74370ddf899c757566
daejin-lee <daejin.lee@nhn.com>
2021-02-02 18:41:25 +0900

    Create example14.js

---
+++ Chap11/example14.js
@@ -0,0 +1,14 @@
+const o = { x: { y: 1 } };
+const c1 = { ...o };
+
+console.log(o);
+console.log(c1);
+
+console.log(c1 === o);
+console.log(c1.x === o.x);
+
+const _ = require('lodash');
+const c2 = _.cloneDeep(o);
+
+console.log(c2 === o);
+console.log(c2.x === o.x);
